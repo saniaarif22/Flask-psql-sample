34a7cc4567e72d9368aa2be8de0f0c534a42c833
Sania Arif <sania.arif@columbia.edu>
2016-04-21 03:18:49 -0400

    backend server file

---
+++ server.py
@@ -0,0 +1,85 @@
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+DATABASEURI = " " #Database uri with username and password goes here
+
+engine = create_engine(DATABASEURI)
+
+# Queries run on database
+
+engine.execute("""CREATE TABLE IF NOT EXISTS test (
+  id serial,
+  name text
+);""")
+engine.execute("""INSERT INTO testtable(name) VALUES ('hi'), ('hello'), ('hey');""")
+
+
+@app.before_request
+def before_request():
+  try:
+    g.conn = engine.connect()
+  except:
+    print "issue in connecting to database"
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+# Routes
+
+@app.route('/')
+def index():
+  print request.args
+
+  cursor = g.conn.execute("SELECT name FROM test")
+  names = []
+  for result in cursor:
+    names.append(result['name'])  
+  cursor.close()
+
+  context = dict(data = names)
+
+  return render_template("index.html", **context)
+
+@app.route('/another')
+def another():
+  return render_template("another.html")
+
+@app.route('/add', methods=['POST'])
+def add():
+  name = request.form['name']
+  g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
+  return redirect('/')
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
